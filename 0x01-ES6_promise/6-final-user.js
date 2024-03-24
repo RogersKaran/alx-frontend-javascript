89f7d1e483bdf61a98629ed18fce90bd6f14480a
RogersKaran <122857486+RogersKaran@users.noreply.github.com>
2024-03-24 15:33:12 +0300

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,15 @@
-import signUpUser from "./4-user-promiser";
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    const promises = [
-        signUpUser(firstName, lastName),
-	uploadPhoto(fileName)
-    ];
-
-    const results = await Promise.allSettled(promises);
-
-    const array = result.map(result => {
-        status: result.staus,
-	value: result.status === 'fulfilled' ? result.value : result.reason
-    });
-
-    return array;
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
+    const arr = [];
+    for (const item of values) {
+      arr.push({ status: item.status, value: item.value || item.reason });
+    }
+    return arr;
+  });
 }
-
