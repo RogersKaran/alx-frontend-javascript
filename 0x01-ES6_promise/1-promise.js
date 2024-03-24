d8460a6dc889830a420b76e1cb5e1914ca2a705a
RogersKaran <122857486+RogersKaran@users.noreply.github.com>
2024-03-24 15:26:07 +0300

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,14 @@
 export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success) {
-	    resolve({ status: 200, body: 'Success' });
+  return new Promise((resolve, reject) => {
+    const object = {
+      status: 200,
+      body: 'Success',
+    };
+    
+    if (success === true) {
+	    resolve(object);
 	} else {
-	    reject(new Error('The fake API is not working currently'));
+	    reject(Error('The fake API is not working currently'));
 	}
     });
 }
-
